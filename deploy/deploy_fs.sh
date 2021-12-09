989b85f27e89518f7cad3d890def998685e03242
Lukas Popp <luk.popp0@gmail.com>
2021-12-09 23:12:19 +0100

    Add and deploy new animations in new file

--- deploy/deploy_fs.sh
+++ deploy/deploy_fs.sh
@@ -10,13 +10,19 @@ command -v pio >/dev/null 2>&1 || { echo >&2 "pio can't be found. Add to PATH!";
 echo "[MPC] prepare /data"
 rm -rf ./data/
 mkdir ./data/
+mkdir ./data/image_data/
 
 echo "[MPC] copying /website to /data"
 cp -rf ./website/* ./data/
 
+
 echo "[MPC] converting to *.gz"
 
 gzip -rf ./data*
+
+echo "[MPC] copying /image_data to /data/image_data"
+
+cp -rf ./src/image_data/pokemon/* ./data/image_data/
 
 pio run --target uploadfs || exit 4
 
